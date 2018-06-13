007dbed11d4220147c4eac301d7a384c3e0014a7
kucheriavii <kucheriavii@gmail.com>
2018-06-13 10:21:46 +0300

    второй вариант адаптивности

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
 $(function() {
-
+    sandwich();
 	// Custom JS
-
+    if ($(document).height() <= $(window).height())
+        $("footer.footer").addClass("navbar-fixed-bottom");
 });
+
+var sandwich = function(){ //Сендвич анимация на кнопку меню для мобильных версий
+    $(".sandwich, .menu_item").click(function() {
+        $(".sandwich").toggleClass("active");
+        if($(".sandwich").hasClass("active")){
+            $('.main-menu').addClass('active');
+        } else{
+            $('.main-menu').removeClass('active');
+        }
+    });
+};
